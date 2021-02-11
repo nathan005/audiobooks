5619bf75bf2698da1ddcf476c3cf08ba8f587d9c
nathan005 <nathanoo5@aol.com>
2021-02-10 20:48:45 -0500

    Update fromcd.sh

--- fromcd.sh
+++ fromcd.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-# Copywrite 2021 Nathan005
+# Copyright 2021 Nathan005
 #
 # This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# it under the terms of the BSD 2-Clause License.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
